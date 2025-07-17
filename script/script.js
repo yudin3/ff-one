1b4addc9a70a1a7ada2a5765c25523a65bcbae26
Artem Yudin <jazzybagg@gmail.com>
2025-07-17 14:31:02 +0300

    Сайт v0.1

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 console.log(document)
 document.addEventListener('DOMContentLoaded', () => {
   'use Srtict';
+  const body = document.getElementById("body");
   const tariffsSwitchers = document.querySelectorAll('.tariffs__label');
   const releaseButtonElems = document.querySelectorAll('.questions__label');
   const buttonCallRequest = document.querySelectorAll('.button__call-request');
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const sectionNavigationMobile = document.querySelector('.navigation-mobile');
   const sectionNavigationMobiletSwithcer = document.querySelector('.navigation-mobile__container-opacity');
   const navigationMobileSwitchers = document.querySelectorAll('.mobile-menu__item');
+  const backToTop = document.getElementById("back-to-top");
 
 /*  const navigationMobiletContent =  document.querySelector('.navigation-mobile__container-content'); */
 
@@ -49,26 +51,47 @@ document.addEventListener('DOMContentLoaded', () => {
    buttonCallRequest.forEach((elem,i) => {
      elem.addEventListener('click', () => {
        sectionCallRequest.classList.toggle('call-request-active');
+       body.classList.toggle('no-scroll');
      });
     });
 
     sectionCallRequestSwithcer.addEventListener('click', () => {
       sectionCallRequest.classList.toggle('call-request-active');
+      body.classList.toggle('no-scroll');
     });
 
 
     buttonNavigationMobile.addEventListener('click', () => {
       sectionNavigationMobile.classList.toggle('navigation-mobile-active');
+      body.classList.toggle('no-scroll');
     });
 
     sectionNavigationMobiletSwithcer.addEventListener('click', () => {
       sectionNavigationMobile.classList.toggle('navigation-mobile-active');
+      body.classList.toggle('no-scroll');
     });
 
     navigationMobileSwitchers.forEach((elem,i) => {
        elem.addEventListener('click', () => {
+         body.classList.toggle('no-scroll');
            sectionNavigationMobile.classList.toggle('navigation-mobile-active');
           });
+    });
+
+    window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 300) {
+            backToTop.style.display = "flex";
+        } else {
+            backToTop.style.display = "none";
+        }
+    });
+
+    backToTop.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
     });
 
 /*    navigationMobiletContent.classList.toggle('navigation-mobile__container-content-active'); */
